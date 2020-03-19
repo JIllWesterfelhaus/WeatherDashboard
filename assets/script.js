41366e1659d6da7a7d33879b371e5809f0e031f6
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-03-19 10:35:26 -0500

    redid city search box and button

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,23 @@
-//building search box
-var citySearchInput = document.querySelector("#citySearch-text");
-var citySearchForm =document.querySelector("#citySearch-form");
-var citySearchList = document.querySelector("#citySearch-list");
-
-var citySearch = ();
-
-init();
-function renderCitySearch() {
-citySearchListHTML = "";
-for (var i = 0; i < citySearch.length;i++) {
-    var citySearch=citySearches[i];
-    var li=document.createElement("li");
-    li.textContent = citySearch;
-    li.setAttribute("data-index",i);
-    var button = document.createElement("button");
-    button.textContent = "Clear History";
-
-    li.appendChild(button);
-    citySearchList.appendChild(li);
+//building city search button
+function addItem() {
+    var newItem = document.createElement("div");
+    newItem.innerHTML = document.getElementById("citySearch").value;
+    newItem.onclick = removeItem;
+    document.getElementById("list").appendChild(newItem);
+    saveList();
 }
+function removeItem() {
+    document.getElementById("list").removeChild(this);
+    saveList();
 }
-
-function init() {
-    var storedCitySearches = JSON.parse(localStorage.getElement("citySearches"));
-    if (storedCitySearches !== null) {
-        citySearches = storedCitySearches;
+function saveList() {
+    localStorage.storedList = document.getElementById("list").innerHTML;
+}
+function loadList() {
+    document.getElementById("list").innerHTML = localStorage.storedList;
+    list.children[i].onclick = removeItem;  
+    if(localStorage.storedList) {
+        loadList();
     }
-    render citySearches();
 }
-
-function storedCitySearches() {
-    localStorage.setItem("citysearches",JSON.stringify(citySearches));
-}
-
-citySearchForm.addEventListener("submit",function(event) {
-    event.preventDefault();
-    var citySearchText = citySearchInput.nodeValue.trim();
-    if(citySearchText ==="") {
-        return;
-    }
-
-    citySearches.push(todoText);
-    citySearchInput.value = "";
-
-    storeCitySearches();
-    renderCitySearches();
-});
-
-citySearchList.addEventListener("click", function(event) {
-var element = event.target;
-
-if (element.matches("button") === true) {
-    var index = element.parentElement.getAttribute("data-index");
-    citySearches.splice(index, 1);
-    storeCitySearches();
-    renderCitySearches();
-}
-});
-
-//search history popping up when search box is clicked.
 
