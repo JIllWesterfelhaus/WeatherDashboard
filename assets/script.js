8673101048047a14e364de35afaaccb4cfa4cd15
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-03-19 07:41:58 -0500

    added html and js, building search box

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var citysearchInput = document.querySelector("#citySearch-text");
+var citySearchForm =document.querySelector("#citySearch-form");
+var citySearchList = document.querySelector("#citySearch-list");
+
+
